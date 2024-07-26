6f3637ea4414e072c710c14da8bade12a324c2ea
BiG ADLER <95148142+BiG-ADLER@users.noreply.github.com>
2024-07-26 13:21:39 +0330

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommentForm from './Commponent/CommentForm/CommentForm';
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <CommentForm></CommentForm>
+    </div>
+  );
+}
+
+export default App;
